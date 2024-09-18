ac70dd6e6826162ead642eb28225ee0984412f5f
mleurino <mleurino8@gmail.com>
2024-09-18 19:06:46 -0300

    FirstProject has started

---
+++ src/app/componentes/top-bar/top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss'
+})
+export class TopBarComponent {
+  constructor(){
+
+  }
+  nombre= "Martino Perez";
+
+}
+
