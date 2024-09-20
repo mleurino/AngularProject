adb7dcb775abad10950b402b4f8633ff529c518e
mleurino <mleurino8@gmail.com>
2024-09-19 22:19:29 -0300

    added top and side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './componentes/top-bar/top-bar.component';
+import { SideBarComponent } from './componentes/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
